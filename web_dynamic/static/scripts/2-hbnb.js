124fc5d791d775f6e98786cb27ecce1d9dd12182
Anne <anne.cognet@holbertonschool.com>
2017-05-04 16:45:54 +0000

    add question 3

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,12 +1,15 @@
 $(document).ready(function () {
-  $.getJSON('http://0.0.0.0:5001/api/v1/status/',
-    function (res) {
-	alert(res.status);
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    dataType: 'json',
+    success: function (res) {
       if (res.status === 'OK') {
         $('DIV#api_status').addClass('available');
       } else {
         $('DIV#api_status').removeClass('available');
       }
+    },
+    error: function (res) {
+        $('DIV#api_status').removeClass('available');
     }
-  );
 });
